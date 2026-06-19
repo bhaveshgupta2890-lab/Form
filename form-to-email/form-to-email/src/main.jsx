2b7bfbee8261aafa9579b7963b78290189bceb00
Bhavesh Gupta <bhavesh.gupta@aapc.com>
2026-06-19 20:52:32 +0530

    code updated

--- form-to-email/form-to-email/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-document.documentElement.style.scrollBehavior = "smooth";
-document.body.style.margin = "0";
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
